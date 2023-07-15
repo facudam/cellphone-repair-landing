6f6d06521060756a0ffc0b0960008216daa09593
facudam <facundo89caceres@gmail.com>
2023-07-15 14:42:18 -0300

    continuación de la creación de NavBar

--- components/ui/NavBar.tsx
+++ components/ui/NavBar.tsx
@@ -4,12 +4,25 @@ import Link from "next/link"
 
 const NavBar = () => {
   return (
-    <nav className='flex py-3.5 px-3.5 border-b'>
-        <Link href='/' className="flex gap-3 justify-center items-center">
-          <Image src='/mobile-repair.png' width='50' height={100} alt="logo" />
-          <span>Reparación de celulares</span>
-        </Link>  
-    </nav>
+    <div className="flex justify-center border-b py-2 px-2">
+      <nav className='flex w-full max-w-7xl sp justify-between items-center '>
+          <Link href='/' className="flex gap-3 justify-center items-center">
+            <Image src='/mobile-repair.png' width='50' height={100} alt="logo" />
+            <span>Reparación de celulares</span>
+          </Link>
+          <ul className="flex gap-7">
+            <li>
+              <Link href='/'>Servicios</Link>  
+            </li>
+            <li>
+              <Link href='/'>Sobre nosotros</Link>
+            </li>
+            <li>
+              <Link href='/'>Contacto</Link>
+            </li>
+          </ul>  
+      </nav>
+   </div>
   )
 }
 
