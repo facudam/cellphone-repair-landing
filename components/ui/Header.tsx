4ac6a0362df8b5c7440f2fb7541b04bc5f240c62
facudam <facundo89caceres@gmail.com>
2023-07-15 21:06:14 -0300

    Diseñando NavBar y Header

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -4,11 +4,10 @@ import React from 'react'
 
 export const Header = () => {
   return (
-    <header className='flex flex-col justify-center items-center heigth-75vh bgd-image relative'>
-        <Image className='h-image' src='/smartphone-arreglo.webp' alt='reparar celular' width={100} height={75} />
-        <h1 className='z-10'>Reparación de celulares en Florencio Varela y zona sur</h1>
-        <p className='z-10'>Expertos en reparación de smartphones. ¡Recupera la funcionalidad de tu celular con nosotros! Rápido, seguro y confiable, Tu celular en buenas manos.</p>
-        <Link className='z-10' href='/'>Nuestros servicios</Link>
+    <header className='flex flex-col justify-center items-center gap-6 heigth-75vh bgd-image text-white'>
+        <h1 className='text-5xl font-bold max-w-3xl text-center'>Reparación de celulares en Florencio Varela y zona sur</h1>
+        <p className='max-w-3xl text-center text-lg'>Expertos en reparación de smartphones. ¡Recupera la funcionalidad de tu celular con nosotros! Rápido, seguro y confiable, Tu celular en buenas manos.</p>
+        <Link className='py-3 px-7 bg-sky-800 uppercase font-semibold' href='/'>Nuestros servicios</Link>
     </header>
   )
 }
--- components/ui/NavBar.tsx
+++ components/ui/NavBar.tsx
@@ -4,11 +4,12 @@ import Link from "next/link"
 
 const NavBar = () => {
   return (
+   <>
     <div className="flex justify-center border-b py-2 px-3">
-      <nav className='flex w-full max-w-7xl sp justify-between items-center '>
-          <Link href='/' className="flex gap-1 justify-center items-center">
+      <nav className='flex w-full max-w-7xl sp justify-between items-center'>
+          <Link href='/' className="flex justify-center items-center">
             <Image src='/mob-repair.png' width='50' height={100} alt="logo" />
-            <span className="uppercase font-semibold">Reparación de celulares</span>
+            <span className="uppercase font-mono text-1xl">Reparación de celulares</span>
           </Link>
           <ul className="flex gap-7 uppercase">
             <li>
@@ -22,7 +23,17 @@ const NavBar = () => {
             </li>
           </ul>  
       </nav>
-   </div>
+    </div>
+    <div className="flex justify-center items-center py-2 gap-3">
+      <Link href='/'>
+        <Image src='/logo-facebook.svg' alt="icono de facebook" width={25} height={25}/>
+      </Link>
+      <Link href='/'>
+        <Image src='/logo-instagram.svg' alt="icono de instagram" width={22} height={25} />
+      </Link> 
+      <Image src='/logo-whatsapp.svg' alt="icono de whatsapp" width={25} height={25} /> 
+    </div>
+   </>
   )
 }
 
