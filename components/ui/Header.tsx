fc889fb68a22389e250814e8381d183f327bdc2e
facudam <facundo89caceres@gmail.com>
2023-07-17 21:06:52 -0300

    Añadiendo nuevas imagenes en logo y header, modificando e instalando fuentes

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -4,18 +4,18 @@ import React from 'react'
 
 export const Header = () => {
   return (
-    <header className='flex items-center flex-wrap gap-5 justify-between w-full max-w-7xl heigth-75vh' >
+    <header className='flex items-center flex-wrap gap-5 justify-between w-full max-w-7xl heigth-75vh text-slate-600' >
         <div className='flex flex-col w-full max-w-xl gap-5'>
-          <h1 className='text-5xl font-bold'>Reparación de celulares en Florencio Varela y zona sur</h1>
-          <p className='max-w-2xl text-lg'>Expertos en reparación de smartphones. ¡Recupera la funcionalidad de tu celular con nosotros! Rápido, seguro y confiable. Tu celular en buenas manos.</p>
+          <h1 className='text-6xl title-weight'>Reparación de celulares en Florencio Varela</h1>
+          <p className='max-w-2xl text-lg text-slate-800'>Expertos en reparación de smartphones en zona sur. ¡Recupera la funcionalidad de tu celular con nosotros! Rápido, seguro y confiable. Tu celular en buenas manos.</p>
           <div className='flex gap-5'>
-            <Link className='py-3 px-7 bg-sky-800 uppercase font-semibold text-white' href='/'>Nuestros servicios</Link>
-            <Link className='py-3 px-7 font-semibold uppercase border border-sky-800 text-sky-800' href=''>Contactanos</Link>
+            <Link className='py-3 px-7 bg-sky-600 uppercase font-semibold text-white' href='/'>Nuestros servicios</Link>
+            <Link className='py-3 px-7 font-semibold uppercase border border-sky-600 text-sky-600' href=''>Contactanos</Link>
           </div>
         </div>
 
-        <div className='bg-gradient-to-r from-cyan-500 to-blue-500'>
-          <Image src='/smartphone.png' width={500} height={100} alt='image' />
+        <div className='w-full max-w-md'>
+          <Image src='/tecnicos-de-celulares.svg' width={500} height={100} alt='image' />
         </div>
     </header>
   )
--- components/ui/NavBar.tsx
+++ components/ui/NavBar.tsx
@@ -5,33 +5,28 @@ import Link from "next/link"
 export const NavBar = () => {
   return (
    <>
-    <div className="flex justify-center py-2 px-3 w-full border-b">
+    <div className="flex justify-center py-2 px-3 w-full bg-gradient text-slate-600">
       <nav className='flex w-full max-w-7xl justify-between items-center'>
           <Link href='/' className="flex justify-center items-center">
-            <Image src='/mob-repair.png' width='50' height={100} alt="logo" />
+            <Image src='/mob-repair.webp' width='50' height={100} alt="logo" />
             <span className="uppercase font-mono text-1xl">Reparación de celulares</span>
           </Link>
+          
           <ul className="flex gap-7 uppercase">
-            <li>
-              <Link href='/'>Servicios</Link>  
-            </li>
-            <li>
-              <Link href='/'>Sobre nosotros</Link>
-            </li>
-            <li>
-              <Link href='/'>Contacto</Link>
-            </li>
-          </ul>  
+            <li><Link href='/'>Servicios</Link></li>
+            <li><Link href='/'>Sobre nosotros</Link></li>
+            <li><Link href='/'>Contacto</Link></li>
+          </ul> 
+          <div className="flex justify-center items-center py-2 gap-3">
+            <Link href='/'>
+              <Image src='/logo-facebook.svg' alt="icono de facebook" width={25} height={25}/>
+            </Link>
+            <Link href='/'>
+              <Image src='/logo-instagram.svg' alt="icono de instagram" width={22} height={25} />
+            </Link> 
+            <Image src='/logo-whatsapp.svg' alt="icono de whatsapp" width={25} height={25} /> 
+          </div>
       </nav>
-    </div>
-    <div className="flex justify-center items-center py-2 gap-3 border-b">
-      <Link href='/'>
-        <Image src='/logo-facebook.svg' alt="icono de facebook" width={25} height={25}/>
-      </Link>
-      <Link href='/'>
-        <Image src='/logo-instagram.svg' alt="icono de instagram" width={22} height={25} />
-      </Link> 
-      <Image src='/logo-whatsapp.svg' alt="icono de whatsapp" width={25} height={25} /> 
     </div>
    </>
   )
